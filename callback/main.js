a43c6161867d3e73ffadcaa7c7d04c0ed99a79c5
ekonuma <konumaeike5618@gmail.com>
2024-01-14 18:44:32 -0300

    feat(callback)

---
+++ callback/main.js
@@ -0,0 +1,24 @@
+function print(callback){
+  const result = callback();
+  console.log(result);
+}
+
+function fn(number = 12)  {
+  return number * 2;
+}
+
+console.log(fn(2));
+console.log(fn(4));
+
+const fnArrow = () => {
+  console.log(number);
+  return number * 2;
+};
+
+const fnArrowObj = (number) => ({ result: number * 2 });
+
+console.log(fnArrowObj(2));
+
+debugger;
+print(fn);
+
