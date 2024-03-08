1cdc7d2fac3d480ebdafb9aa5872f9225141a1d7
ekonuma <konumaeike5618@gmail.com>
2024-03-07 21:22:31 -0300

    feat(multi checkbox)

--- react-basic/06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ react-basic/06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -8,10 +8,41 @@ const Example = () => {
   ]);
 
   const [sum, setSum] = useState(0);
+  const handleChange = (e) => {
+    const newFruits = fruits.map((fruit) => {
+      const copyFruit = { ...fruit };
+      if (copyFruit.label === e.target.value) {
+        copyFruit.checked = !fruit.checked;
+      }
+      return copyFruit;
+    });
+    setFruits(newFruits);
 
+    let sum = newFruits
+      .filter((fruit) => fruit.checked)
+      .reduce((sumVal, fruit) => sumVal + fruit.value, 0);
+    setSum(sum);
+  };
   return (
     <div>
-      
+      {fruits.map((fruit) => {
+        return (
+          <>
+            <div key={fruit.label}>
+              <input
+                id={fruit.label}
+                type="checkbox"
+                value={fruit.label}
+                checked={fruit.checked}
+                onChange={handleChange}
+              />
+              <label htmlFor={fruit.label}>
+                {fruit.label}:{fruit.value}
+              </label>
+            </div>
+          </>
+        );
+      })}
       <div>合計：{sum}</div>
     </div>
   );
