8ad482f022c888ace9e665e899602065303d884c
ekonuma <konumaeike5618@gmail.com>
2024-03-10 21:37:28 -0300

    feat(css): inline, external, module and css-in-js

--- react-basic/07_styling_component/src/040_css_in_js/start/Example.js
+++ react-basic/07_styling_component/src/040_css_in_js/start/Example.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
-
+import styled from "styled-components";
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
-
+  const StyledBtn = styled.button`
+    margin: auto;
+    border-radius: 9999px;
+    border: none;
+    display: block;
+    width: 120px;
+    height: 60px;
+    font-weight: bold;
+    cursor: pointer;
+    background: ${({ isSelected }) => (isSelected ? "pink" : "")};
+  `;
   return (
     <>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
+      <StyledBtn isSelected={isSelected} onClick={clickHandler}>
         ボタン
-      </button>
+      </StyledBtn>
       <div style={{ textAlign: "center" }}>
         {isSelected && "クリックされました。"}
       </div>
