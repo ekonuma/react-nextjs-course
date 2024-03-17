dff2993aeb488133a7881bfcc36c195f7107e219
ekonuma <konumaeike5618@gmail.com>
2024-03-16 23:23:05 -0300

    feat(foward ref)

--- react-basic/08_other_function/src/060_practice_ref/start/Example.js
+++ react-basic/08_other_function/src/060_practice_ref/start/Example.js
@@ -1,14 +1,20 @@
 import { useState, useRef, forwardRef, useImperativeHandle } from "react";
 
-const Video = forwardRef(({ path }, ref) => {
+const Video = forwardRef((props, ref) => {
+  const videoRef = useRef();
 
   useImperativeHandle(ref, () => ({
-    
+    pause() {
+      videoRef.current.pause();
+    },
+    play() {
+      videoRef.current.play();
+    },
   }));
 
   return (
-    <video style={{ maxWidth: "100%" }}>
-      <source src={path}></source>
+    <video style={{ maxWidth: "100%" }} ref={videoRef}>
+      <source src={props.path}></source>
     </video>
   );
 });
@@ -21,11 +27,17 @@ const Example = () => {
   return (
     <div>
       <h3>練習問題</h3>
-      <p>useRef、useImperativeHandle、forwardRefを使って完成系の動画再生機能を作成してください。※useImperativeHandleでplay(再生)、stop(停止)メソッドを定義すること。
-</p>
+      <p>
+        useRef、useImperativeHandle、forwardRefを使って完成系の動画再生機能を作成してください。※useImperativeHandleでplay(再生)、stop(停止)メソッドを定義すること。
+      </p>
       <Video ref={ref} path="./sample.mp4" />
       <button
         onClick={() => {
+          if (playing) {
+            ref.current.pause();
+          } else {
+            ref.current.play();
+          }
           setPlaying((prev) => !prev);
         }}
       >
